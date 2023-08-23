fc373a460b4e118df94355fd077c1a2c43a724f3
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-23 15:25:07 +0200

    task 3 - problem with notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,27 @@
+// import * as Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      // Resolve
+      setTimeout(() => {
+        resolve({ position, delay });
+      }, delay);
+    } else {
+      // Reject
+      setTimeout(() => {
+        reject({ position, delay });
+      }, delay);
+    }
+  });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
