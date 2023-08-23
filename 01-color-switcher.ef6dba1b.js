164fb78da9e9a5710eb9ff8755419daddf57bfe4
KaczorowskaKarolina <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-23 11:42:02 +0000

    Deploying to gh-pages from @ KaczorowskaKarolina/goit-js-hw-09@b786cd51d20fafe77db7b01a030521f24d3df0ab 🚀

---
+++ 01-color-switcher.ef6dba1b.js
@@ -0,0 +1,2 @@
+window.onload=function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),o=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));o.style.backgroundColor=t}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))};
+//# sourceMappingURL=01-color-switcher.ef6dba1b.js.map
