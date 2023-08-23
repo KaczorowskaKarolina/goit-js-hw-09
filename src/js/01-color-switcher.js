b786cd51d20fafe77db7b01a030521f24d3df0ab
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-23 13:41:33 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+window.onload = function() {
+  var startButton = document.querySelector('[data-start]');
+  var stopButton = document.querySelector('[data-stop]');
+  var body = document.querySelector('body');
+  var intervalId;
+
+  function start() {
+    intervalId = setInterval(function() {
+      var color = getRandomHexColor();
+      body.style.backgroundColor = color;
+    }, 1000);
+    startButton.disabled = true;
+  }
+
+  function stop() {
+    clearInterval(intervalId);
+    startButton.disabled = false;
+  }
+
+  startButton.addEventListener('click', start);
+  stopButton.addEventListener('click', stop);
+};
