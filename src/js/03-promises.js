36b2592c062a949f53a2c9debeb635d24b07e8cc
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-24 16:16:10 +0200

    delete errors

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,32 @@
-// import * as Notiflix from 'notiflix';
+// import Notiflix from 'notiflix';
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   return new Promise((resolve, reject) => {
+//     if (shouldResolve) {
+//       // Resolve
+//       setTimeout(() => {
+//         resolve({ position, delay });
+//       }, delay);
+//     } else {
+//       // Reject
+//       setTimeout(() => {
+//         reject({ position, delay });
+//       }, delay);
+//     }
+//   });
+
+// }
+// for(let i = 1; i <= 5; i++) { const delay = Math.floor(Math.random() * 3000) + 1000;
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+// }
+
 import Notiflix from 'notiflix';
 
 function createPromise(position, delay) {
@@ -16,12 +44,7 @@ function createPromise(position, delay) {
       }, delay);
     }
   });
+
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+for(let i = 1; i <= 5; i++) { const delay = Math.floor(Math.random() * 3000) + 1000; createPromise(i, delay) .then(({ position, delay }) => { Notiflix.Notify.success('✅ Fulfilled promise ${position} in ${delay}ms'); }) .catch(({ position, delay }) => { Notiflix.Notify.failure('❌ Rejected promise ${position} in ${delay}ms'); }); }
