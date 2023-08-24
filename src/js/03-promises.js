c36ee158413c1836bb3b466a74c2a349086d3c03
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-24 17:12:45 +0200

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,50 +1,43 @@
-// import Notiflix from 'notiflix';
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   return new Promise((resolve, reject) => {
-//     if (shouldResolve) {
-//       // Resolve
-//       setTimeout(() => {
-//         resolve({ position, delay });
-//       }, delay);
-//     } else {
-//       // Reject
-//       setTimeout(() => {
-//         reject({ position, delay });
-//       }, delay);
-//     }
-//   });
-
-// }
-// for(let i = 1; i <= 5; i++) { const delay = Math.floor(Math.random() * 3000) + 1000;
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-// }
-
 import Notiflix from 'notiflix';
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      // Resolve
-      setTimeout(() => {
-        resolve({ position, delay });
-      }, delay);
-    } else {
-      // Reject
-      setTimeout(() => {
-        reject({ position, delay });
-      }, delay);
-    }
-  });
+    const shouldResolve = Math.random() > 0.3;
 
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Resolve
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
-for(let i = 1; i <= 5; i++) { const delay = Math.floor(Math.random() * 3000) + 1000; createPromise(i, delay) .then(({ position, delay }) => { Notiflix.Notify.success('✅ Fulfilled promise ${position} in ${delay}ms'); }) .catch(({ position, delay }) => { Notiflix.Notify.failure('❌ Rejected promise ${position} in ${delay}ms'); }); }
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const initialDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    const delay = initialDelay + (step * (i - 1));
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
